1b1ee996ad3b5dabceaf337ef7063408c5e3bcdd
Flen-E <cjj642@naver.com>
2023-08-26 22:46:01 +0900

    chart

--- src/routes/Chart.tsx
+++ src/routes/Chart.tsx
@@ -1,6 +1,93 @@
-function Chart(){
+import { useParams } from "react-router-dom";
+import { fetchCoinHistory } from "../api";
+import { useQuery } from "react-query";
+import ApexCharts from "react-apexcharts";
+
+interface IHistoricalData{
+    close : string;
+    high : string;
+    low : string;
+    market_cap : string;
+    open : string;
+    time_close : number;
+    time_open : number;
+    volume : string;
+}
+interface CharProps {
+    coinId : string;
+}
+function Chart({coinId} : CharProps){
+    const {isLoading, data} = useQuery<IHistoricalData[]>(["ohlcv",coinId], () => fetchCoinHistory(coinId))
+    
     return (
-        <h1>Chart</h1>
+        <div>
+            {isLoading ? "Loading chart..." :
+                <ApexCharts 
+                type="line" 
+                series = {[
+                    {
+                        name : "Price",
+                        data : data?.map(price => parseFloat(price.close)) ?? [] ,
+                    },
+                ]}
+                options={{
+                    theme: {
+                        mode : "dark"
+                    },
+                    
+                    chart: {
+                        height : 500,
+                        width: 500,
+                        toolbar : {
+                            show : false,
+                        },
+                        background : "transparent", 
+                    },
+                    stroke: {
+                        curve : "smooth",
+                        width : 4,
+
+                    },
+                    yaxis : {
+                        show: false,
+                        
+                    },
+                    xaxis : {
+                        labels : {
+                            show: false,
+                        },
+                        axisTicks : {
+                            show: false,
+                        },
+                        axisBorder : {
+                            show: false,
+                        },
+                        categories: data?.map((price) =>
+                            new Date(price.time_close * 1000).toUTCString()
+                        ),
+                        
+                    },
+                    fill : {
+                        type : "gradient",
+                        gradient : {
+                            gradientToColors : ["#0be881"],
+                            stops: [0, 100],
+
+                        },
+                    },
+                    colors : ["#0fbcf9"],
+                    tooltip : {
+                        y : {
+                            formatter : (value) => `$ ${value.toFixed(3)}`
+                        }
+                    }
+                
+                }}>
+
+                </ApexCharts>
+            }
+        </div>
     )
+
 }
 export default Chart;
